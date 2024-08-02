c115dc80cebdfd570b0dcbc8fb3ec1c62f36dd6b
Marina Kharitonova <6172331@mail.ru>
2024-08-02 17:18:09 +0300

    feat: add jest

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2020: true, 'jest/globals': true },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
@@ -9,6 +9,7 @@ module.exports = {
     'plugin:react-hooks/recommended',
     'plugin:prettier/recommended',
     'plugin:i18next/recommended',
+    "plugin:jest-dom/recommended"
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
@@ -18,7 +19,7 @@ module.exports = {
     project: ['./tsconfig.json', './tsconfig.node.json', './tsconfig.app.json'],
     tsconfigRootDir: __dirname,
   },
-  plugins: ['react-refresh', 'react', '@typescript-eslint', 'prettier', 'i18next'],
+  plugins: ['react-refresh', 'react', '@typescript-eslint', 'prettier', 'i18next', "jest", "jest-dom"],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
